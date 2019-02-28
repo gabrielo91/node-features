b18b11a39df8efe6890cf817addea08f9fee54e8
Gabriel Ortega Rosero <gabrielo_9103@hotmail.com>
2019-02-28 18:12:39 -0500

    commit sobre master

---
+++ index_modules.js
@@ -0,0 +1,17 @@
+'use strict'
+
+function A(){
+    console.log('A')
+}
+
+function B(){
+    console.log('B')
+}
+
+function C(){
+    console.log('C')
+}
+
+module.exports.A=A
+module.exports.B=B
+module.exports.C=C
